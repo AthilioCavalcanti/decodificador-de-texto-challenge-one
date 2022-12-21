e0a537bf7b397d68bd605f84755b5a47e5f63524
Athílio Cavalcanti <61874357+AthilioCavalcanti@users.noreply.github.com>
2022-12-21 11:04:50 -0300

    :tada: Commit initial

---
+++ assets/javascript/main.js
@@ -0,0 +1,46 @@
+const input = document.querySelector('textarea#text-input-field');
+const output = document.querySelector('div.text-output-field');
+const btnEncrypt = document.querySelector('button.btn-encrypt');
+const btnDecrypt = document.querySelector('button.btn-decrypt');
+const btnCopy =  document.createElement('button');
+
+btnCopy.innerText = 'Copiar';
+btnCopy.classList.add('btn');
+
+const encrypt = (text) => {
+  return text
+        .replaceAll('a', 'ai')
+        .replaceAll('e', 'enter')
+        .replaceAll('i', 'imes')
+        .replaceAll('o', 'ober')
+        .replaceAll('u', 'ufat');
+}
+
+const decrypt = (text) => {
+  return text
+        .replaceAll('ai', 'a')
+        .replaceAll('enter', 'e')
+        .replaceAll('imes', 'i')
+        .replaceAll('ober', 'o')
+        .replaceAll('ufat', 'u');
+}
+
+btnEncrypt.addEventListener('click', () => {
+  output.innerHTML = `<div class="text-output">${encrypt(input.value)}</div>`;
+  output.append(btnCopy);
+});
+
+btnDecrypt.addEventListener('click', () => {
+  output.innerHTML = `<div class="text-output">${decrypt(input.value)}</div>`;
+  output.append(btnCopy);
+});
+
+btnCopy.addEventListener('click', () => {
+  const text = document.querySelector('div.text-output').innerText;
+  navigator.clipboard.writeText(text);
+});
+
+
+// Adicionar função para verificar se a entrada é em letras minusculas e sem acentos
+// Estilizar para telas maiores (tablet, desktop, telas grades...)
+// 
